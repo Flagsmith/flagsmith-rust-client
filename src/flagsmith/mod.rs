4f63ead2cb11075ab60927e15cecc20efea443bd
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2022-04-04 11:38:33 +0530

    wip: add async t

--- src/flagsmith/mod.rs
+++ src/flagsmith/mod.rs
@@ -1,28 +1,34 @@
-use reqwest::header::{self, HeaderMap};
-use std::{collections::HashMap, fmt::{self, format}, string, thread, time::Duration};
-use std::sync::{Arc, Mutex};
 use flagsmith_flag_engine::environments::builders::build_environment_struct;
 use flagsmith_flag_engine::environments::Environment;
-mod models;
+use reqwest::header::{self, HeaderMap};
+use std::sync::{Arc, Mutex};
+use std::{
+    collections::HashMap,
+    fmt::{self, format},
+    string, thread,
+    time::Duration,
+};
 mod analytics;
+mod models;
 const DEFAULT_API_URL: &str = "https://api.flagsmith.com/api/v1/";
 use super::error;
-pub struct FlagsmithOptions{
+pub struct FlagsmithOptions {
     pub api_url: String,
     pub custom_headers: HeaderMap,
     pub request_timeout_seconds: u64,
-    pub environment_refresh_interval: u32,
-    pub enable_analytics: bool
-
+    pub enable_local_evaluation: bool,
+    pub environment_refresh_interval_seconds: u64,
+    pub enable_analytics: bool,
 }
-impl Default for FlagsmithOptions{
-    fn default() -> Self{
-        FlagsmithOptions{
+impl Default for FlagsmithOptions {
+    fn default() -> Self {
+        FlagsmithOptions {
             api_url: DEFAULT_API_URL.to_string(),
             custom_headers: header::HeaderMap::new(),
             request_timeout_seconds: 60,
+            enable_local_evaluation: false,
             enable_analytics: false,
-            environment_refresh_interval: 10
+            environment_refresh_interval_seconds: 10,
         }
     }
 }
@@ -33,36 +39,30 @@ pub struct Flagsmith {
     environment_url: String,
     options: FlagsmithOptions,
     datastore: Arc<Mutex<DataStore>>,
-    environment: Option<Environment>
-
-    //  environment_key: String,
-// api_url: String,
-//custom_headers: HashMap<String, String>,
-//request_timeout_seconds: u8,
-// enable_local_evaluation: bool,
-//environment_refresh_interval_seconds: u32,
-//retries: u8,
-    //enable_analytics: bool, //TODO: Add default flag handler
-
+    environment: Option<Environment>, //  environment_key: String,
+                                      // api_url: String,
+                                      //custom_headers: HashMap<String, String>,
+                                      //request_timeout_seconds: u8,
+                                      // enable_local_evaluation: bool,
+                                      //environment_refresh_interval_seconds: u32,
+                                      //retries: u8,
+                                      //enable_analytics: bool, //TODO: Add default flag handler
 }
-struct DataStore{
-    environment: Option<Environment>
-
+struct DataStore {
+    environment: Option<Environment>,
 }
 impl Flagsmith {
     pub fn new(
         environment_key: String,
-        flagsmith_options: FlagsmithOptions
-        // api_url: Option<String>,
-        // custom_headers: Option<HeaderMap>,
-        // request_timeout_seconds: Option<u64>,
-        // enable_local_evaluation: Option<bool>,
-        // environment_refresh_interval_seconds: Option<u32>,
-        // retries: Option<u8>,
-        // enable_analytics: Option<bool>, // TODO: Add this default_flag_handler:
-
+        flagsmith_options: FlagsmithOptions, // api_url: Option<String>,
+                                             // custom_headers: Option<HeaderMap>,
+                                             // request_timeout_seconds: Option<u64>,
+                                             // enable_local_evaluation: Option<bool>,
+                                             // environment_refresh_interval_seconds: Option<u32>,
+                                             // retries: Option<u8>,
+                                             // enable_analytics: Option<bool>, // TODO: Add this default_flag_handler:
     ) -> Flagsmith {
-        let mut headers = flagsmith_options.custom_headers.clone();//custom_headers.unwrap_or(header::HeaderMap::new());
+        let mut headers = flagsmith_options.custom_headers.clone(); //custom_headers.unwrap_or(header::HeaderMap::new());
         headers.insert(
             "X-Environment-Key",
             header::HeaderValue::from_str(&environment_key).unwrap(),
@@ -70,12 +70,14 @@ impl Flagsmith {
         let timeout = Duration::from_secs(flagsmith_options.request_timeout_seconds);
         let client = reqwest::blocking::Client::builder()
             .default_headers(headers)
-            .timeout(timeout).build().unwrap();
+            .timeout(timeout)
+            .build()
+            .unwrap();
 
         let environment_flags_url = format!("{}flags/", flagsmith_options.api_url);
-        let identities_url = format!("{}identities/",flagsmith_options.api_url);
+        let identities_url = format!("{}identities/", flagsmith_options.api_url);
         let environment_url = format!("{}environment-document/", flagsmith_options.api_url);
-        let ds = Arc::new(Mutex::new(DataStore{environment:None}));
+        let ds = Arc::new(Mutex::new(DataStore { environment: None }));
         let flagsmith = Flagsmith {
             client: client.clone(),
             environment_flags_url,
@@ -83,27 +85,33 @@ impl Flagsmith {
             identities_url: identities_url,
             options: flagsmith_options,
             environment: None,
-                datastore: Arc::clone(&ds)
+            datastore: Arc::clone(&ds),
         };
-        let ds = Arc::clone(&ds);
-        thread::spawn(move ||{ loop{
+        let environment_refresh_interval_seconds = flagsmith.options.environment_refresh_interval_seconds;
+        if flagsmith.options.enable_local_evaluation {
+            let ds = Arc::clone(&ds);
+            thread::spawn(move || loop {
                 println!("updating environment From Thread");
-                let  environment = Some(get_environment_from_api(client.clone(), environment_url.clone()).unwrap());
-                let mut data =  ds.lock().unwrap();
+                let environment = Some(
+                    get_environment_from_api(client.clone(), environment_url.clone()).unwrap(),
+                );
+                let mut data = ds.lock().unwrap();
                 data.environment = environment;
-                //flagsmith.update_environment();
-                thread::sleep(Duration::from_millis(50));
-            }
-        });
+                thread::sleep(Duration::from_secs(environment_refresh_interval_seconds));
+            });
+        }
         return flagsmith;
     }
-    pub fn update_environment(&mut self) -> Result<(), error::Error>{
+    pub fn update_environment(&mut self) -> Result<(), error::Error> {
         println!("Updating environment from main thread");
         let mut data = self.datastore.lock().unwrap();
-        data.environment = Some(get_environment_from_api(self.client.clone(), self.environment_url.clone())?);
+        data.environment = Some(get_environment_from_api(
+            self.client.clone(),
+            self.environment_url.clone(),
+        )?);
         return Ok(());
     }
-    pub fn get_environment_flags(&self) -> models::Flags{
+    pub fn get_environment_flags(&self) -> models::Flags {
         let data = self.datastore.lock().unwrap();
         let environment = data.environment.as_ref().unwrap();
 
@@ -111,24 +119,29 @@ impl Flagsmith {
         // }
 
         return self.get_environment_flags_from_document(environment);
-
     }
     fn get_environment_flags_from_document(&self, environment: &Environment) -> models::Flags {
-        return models::Flags::from_feature_states(&environment.feature_states, None)
+        return models::Flags::from_feature_states(&environment.feature_states, None);
     }
 }
-fn get_environment_from_api(client: reqwest::blocking::Client, environment_url: String) -> Result<Environment, error::Error>{
+fn get_environment_from_api(
+    client: reqwest::blocking::Client,
+    environment_url: String,
+) -> Result<Environment, error::Error> {
     let method = reqwest::Method::GET;
-    let json_document = get_json_response(client,method, environment_url)?;
+    let json_document = get_json_response(client, method, environment_url)?;
     let environment = build_environment_struct(json_document);
-    return Ok(environment)
-
+    return Ok(environment);
 }
-fn get_json_response(client: reqwest::blocking::Client, method: reqwest::Method, url: String) -> Result<serde_json::Value, error::Error>{
+fn get_json_response(
+    client: reqwest::blocking::Client,
+    method: reqwest::Method,
+    url: String,
+) -> Result<serde_json::Value, error::Error> {
     let response = client.request(method, url).send()?;
-    if response.status().is_success(){
-        return Ok(response.json()?)
-    }else {
-        return Err(error::Error::from("Request returned non 2xx".to_string()))
+    if response.status().is_success() {
+        return Ok(response.json()?);
+    } else {
+        return Err(error::Error::from("Request returned non 2xx".to_string()));
     }
 }

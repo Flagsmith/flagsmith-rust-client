7dcb2bad9cd873c0a336a94ea5c8414d8dd4d9ed
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2022-04-04 18:38:16 +0530

    rewrite error module

--- src/error.rs
+++ src/error.rs
@@ -5,50 +5,46 @@ use std::fmt;
 #[derive(Debug)]
 pub struct Error {
     kind: ErrorKind,
-    desc: String,
+    msg: String,
 }
 
 /// Defines error kind.
 #[derive(Debug)]
 pub enum ErrorKind {
-    ParseError,
-    RequestError,
-    AppError,
+    FlagsmithClientError,
+    FlagsmithAPIError,
 }
-
+impl Error{
+    pub fn new(kind: ErrorKind, msg: String) -> Error{
+        Error{
+            kind,
+            msg
+        }
+    }
+}
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self.kind {
-            ErrorKind::ParseError => write!(f, "URL parsing error: {}", &self.desc),
-            ErrorKind::RequestError => write!(f, "REST API request error: {}", &self.desc),
-            ErrorKind::AppError => write!(f, "Application error: {}", &self.desc),
+            ErrorKind::FlagsmithClientError => write!(f, "Flagsmith API error: {}", &self.msg),
+            ErrorKind::FlagsmithAPIError => write!(f, "Flagsmith client error: {}", &self.msg),
         }
     }
 }
 
 impl From<url::ParseError> for Error {
     fn from(e: url::ParseError) -> Self {
-        Error {
-            kind: ErrorKind::ParseError,
-            desc: e.to_string(),
-        }
+        Error::new(ErrorKind::FlagsmithClientError, e.to_string())
     }
 }
 
 impl From<reqwest::Error> for Error {
     fn from(e: reqwest::Error) -> Self {
-        Error {
-            kind: ErrorKind::RequestError,
-            desc: e.to_string(),
-        }
+        Error::new(ErrorKind::FlagsmithAPIError, e.to_string())
     }
 }
 
-impl From<String> for Error {
-    fn from(s: String) -> Self {
-        Error {
-            kind: ErrorKind::AppError,
-            desc: s,
-        }
+impl  From<serde_json::Error> for Error {
+    fn from(e: serde_json::Error) -> Self {
+        Error::new(ErrorKind::FlagsmithAPIError, e.to_string())
     }
 }

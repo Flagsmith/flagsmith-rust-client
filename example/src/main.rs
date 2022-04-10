2c95125a3885e41a477edc32b4e3a32b87575c12
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2022-04-10 19:49:08 +0530

    update example with default handler

--- example/src/main.rs
+++ example/src/main.rs
@@ -12,7 +12,7 @@ use std::env;
 
 use rocket_contrib::templates::Template;
 
-use flagsmith::{Flagsmith, FlagsmithOptions};
+use flagsmith::{Flag, Flagsmith, FlagsmithOptions};
 use flagsmith_flag_engine::identities::Trait;
 use flagsmith_flag_engine::types::{FlagsmithValue, FlagsmithValueType};
 
@@ -22,46 +22,54 @@ struct TemplateContext {
     button_colour: String,
     identifier: String,
 }
+fn default_flag_handler(feature_name: &str) -> Flag {
+    let mut flag: Flag = Default::default();
+    if feature_name == "secret_button" {
+        flag.value.value_type = FlagsmithValueType::String;
+        flag.value.value = serde_json::json!({"colour": "#b8b8b8"}).to_string();
+    }
+    return flag;
+}
 
-#[derive(Serialize)]
-struct  Dumb{}
+use std::{thread, time::Duration};
 #[get("/?<identifier>&<trait_key>&<trait_value>")]
 fn home(
     identifier: Option<String>,
     trait_key: Option<String>,
     trait_value: Option<String>,
 ) -> Template {
+    let options = FlagsmithOptions {
+        default_flag_handler: Some(default_flag_handler),
+        enable_local_evaluation: true,
+        ..Default::default()
+    };
 
-     let flagsmith = Flagsmith::new(
-         env::var("ENVIRONMENT_KEY").unwrap(),
-         FlagsmithOptions::default(),
-     );
-     let flags;
-     if identifier.is_some() {
-         let traits = match trait_key {
-             Some(trait_key) => {
-                 Some(vec![Trait {
-                     trait_key,
-                     trait_value: FlagsmithValue {
-                         value: trait_value.unwrap_or("".to_string()),
-                         value_type: FlagsmithValueType::String,
-                     },
-                 }])
-             }
-             None => None,
-         };
-         flags = flagsmith
-             .get_identity_flags(identifier.as_ref().unwrap().to_string(), traits)
-             .unwrap();
-     } else {
-         // Get the default flags for the current environment
-         flags = flagsmith.get_environment_flags().unwrap();
-     }
+    let flagsmith = Flagsmith::new(env::var("ENVIRONMENT_KEY").unwrap(), options);
+    let flags;
+    if identifier.is_some() {
+        let traits = match trait_key {
+            Some(trait_key) if trait_key != "".to_string() => Some(vec![Trait {
+                trait_key,
+                trait_value: FlagsmithValue {
+                    value: trait_value.unwrap_or("".to_string()),
+                    value_type: FlagsmithValueType::None,
+                },
+            }]),
+            Some(_) => None,
+            None => None,
+        };
+        flags = flagsmith
+            .get_identity_flags(identifier.as_ref().unwrap().to_string(), traits)
+            .unwrap();
+    } else {
+        // Get the default flags for the current environment
+        flags = flagsmith.get_environment_flags().unwrap();
+    }
 
-     let show_button = flags.is_feature_enabled("secret_button").unwrap();
-     let button_data = flags.get_feature_value_as_string("secret_button").unwrap();
+    let show_button = flags.is_feature_enabled("secret_button").unwrap();
+    let button_data = flags.get_feature_value_as_string("secret_button").unwrap();
 
-     let button_json: serde_json::Value = serde_json::from_str(&button_data).unwrap();
+    let button_json: serde_json::Value = serde_json::from_str(&button_data).unwrap();
     let button_colour = button_json["colour"].as_str().unwrap().to_string();
 
     let context = TemplateContext {

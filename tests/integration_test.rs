2e0f8ee1608eb36ebea91c12fb41e4a1a847881f
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2022-04-13 11:58:56 +0530

    wip: Add integration tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,147 +1,219 @@
-use flagsmith::{Client, User, Value};
+use flagsmith_flag_engine::identities::Trait;
+use flagsmith_flag_engine::types::{FlagsmithValue, FlagsmithValueType};
 
-const API_KEY: &str = "MgfUaRCvvZMznuQyqjnQKt";
-const TEST_FEATURE_NAME: &str = "test_feature";
-const TEST_FEATURE_VALUE: &str = "sample feature value";
-const TEST_USER_FEATURE_VALUE: &str = "user feature value";
-const TEST_FLAG_NAME: &str = "test_flag";
-const TEST_FLAG_VALUE: bool = true;
-const TEST_TRAIT_NAME: &str = "test_trait";
-const TEST_TRAIT_VALUE: &str = "sample trait value";
-const TEST_TRAIT_NEW_VALUE: &str = "new value";
-const INVALID_NAME: &str = "invalid_name_for_tests";
-const TEST_DISABLED_FLAG: &str = "disabled_flag";
+use flagsmith::{Flagsmith, FlagsmithOptions};
 
-fn test_user() -> User {
-    User {
-        identifier: String::from("test_user"),
-    }
-}
-fn different_user() -> User {
-    User {
-        identifier: String::from("different_user"),
-    }
+const ENVIRONMENT_KEY: &str = "ser.test_environment_key";
+
+mod fixtures;
+use fixtures::environment_json;
+use fixtures::flags_json;
+use fixtures::identities_json;
+use httpmock::prelude::*;
+use rstest::*;
+
+#[fixture]
+fn mock_server() -> MockServer {
+    MockServer::start()
 }
 
-#[test]
-fn test_get_features() {
-    let features = Client::new(API_KEY).get_features().unwrap();
-    assert_eq!(features.len(), 5);
-    for f in features.iter() {
-        assert!(f.feature.name != "");
-    }
+#[rstest]
+fn test_get_environment_flags_uses_local_environment_when_available(
+    mock_server: MockServer,
+    environment_json: serde_json::Value,
+) {
+    // Given
+    let api_mock = mock_server.mock(|when, then| {
+        when.method(GET)
+            .path("/api/v1/environment-document/")
+            .header("X-Environment-Key", ENVIRONMENT_KEY);
+        then.status(200).json_body(environment_json);
+    });
+    let url = mock_server.url("/api/v1/");
+    let flagsmith_options = FlagsmithOptions {
+        api_url: url,
+        ..Default::default()
+    };
+    let mut flagsmith = Flagsmith::new(ENVIRONMENT_KEY.to_string(), flagsmith_options);
+
+    // When
+
+    flagsmith.update_environment().unwrap();
+
+    // Then
+    let all_flags = flagsmith.get_environment_flags().unwrap().all_flags();
+    assert_eq!(all_flags.len(), 1);
+    assert_eq!(all_flags[0].feature_name, fixtures::FEATURE_1_NAME);
+    assert_eq!(all_flags[0].feature_id, fixtures::FEATURE_1_ID);
+    assert_eq!(
+        all_flags[0].value_as_string().unwrap(),
+        fixtures::FEATURE_1_STR_VALUE
+    );
+    api_mock.assert();
 }
 
-#[test]
-fn test_get_user_features() {
-    let features = Client::new(API_KEY)
-        .get_user_features(&test_user())
-        .unwrap();
-    for f in features.iter() {
-        assert!(f.feature.name != "");
-    }
+#[rstest]
+fn test_get_environment_flags_calls_api_when_no_local_environment(
+    mock_server: MockServer,
+    flags_json: serde_json::Value,
+) {
+    // Given
+    let api_mock = mock_server.mock(|when, then| {
+        when.method(GET)
+            .path("/api/v1/flags/")
+            .header("X-Environment-Key", ENVIRONMENT_KEY);
+        then.status(200).json_body(flags_json);
+    });
+    let url = mock_server.url("/api/v1/");
+    let flagsmith_options = FlagsmithOptions {
+        api_url: url,
+        ..Default::default()
+    };
+    let flagsmith = Flagsmith::new(ENVIRONMENT_KEY.to_string(), flagsmith_options);
+
+    // When
+    let all_flags = flagsmith.get_environment_flags().unwrap().all_flags();
+
+    // Then
+    assert_eq!(all_flags.len(), 1);
+    assert_eq!(all_flags[0].feature_name, fixtures::FEATURE_1_NAME);
+    assert_eq!(all_flags[0].feature_id, fixtures::FEATURE_1_ID);
+    assert_eq!(
+        all_flags[0].value_as_string().unwrap(),
+        fixtures::FEATURE_1_STR_VALUE
+    );
+    api_mock.assert();
 }
+#[rstest]
+fn test_get_identity_flags_uses_local_environment_when_available(
+    mock_server: MockServer,
+    environment_json: serde_json::Value,
+) {
+    // Given
+    let api_mock = mock_server.mock(|when, then| {
+        when.method(GET)
+            .path("/api/v1/environment-document/")
+            .header("X-Environment-Key", ENVIRONMENT_KEY);
+        then.status(200).json_body(environment_json);
+    });
+    let url = mock_server.url("/api/v1/");
+    let flagsmith_options = FlagsmithOptions {
+        api_url: url,
+        ..Default::default()
+    };
+    let mut flagsmith = Flagsmith::new(ENVIRONMENT_KEY.to_string(), flagsmith_options);
 
-#[test]
-fn test_has_value() {
-    let client = Client::new(API_KEY);
-    let ok = client.has_feature(TEST_FEATURE_NAME).unwrap();
-    assert!(ok);
+    // When
 
-    let ok = client.has_feature(INVALID_NAME).unwrap();
-    assert!(ok == false);
-}
+    flagsmith.update_environment().unwrap();
 
-#[test]
-fn test_has_user_value() {
-    let client = Client::new(API_KEY);
-    let ok = client.has_user_feature(&test_user(), TEST_FEATURE_NAME).unwrap();
-    assert!(ok);
-
-    let ok = client.has_user_feature(&test_user(), INVALID_NAME).unwrap();
-    assert!(ok == false);
-}
-
-#[test]
-fn test_feature_enabled() {
-    let client = Client::new(API_KEY);
-    let enabled = client.feature_enabled(TEST_FLAG_NAME).unwrap();
-    assert!(enabled);
-}
-
-#[test]
-fn test_feature_disabled() {
-    let client = Client::new(API_KEY);
-    let enabled = client.feature_enabled(TEST_DISABLED_FLAG).unwrap();
-    assert!(!enabled);
-}
-
-#[test]
-fn test_get_value() {
-    let client = Client::new(API_KEY);
-    let val = client.get_value(TEST_FEATURE_NAME).unwrap().unwrap();
-    match val {
-        Value::String(v) => assert!(v == TEST_FEATURE_VALUE),
-        _ => assert!(false),
-    }
-
-    let val = client.get_value("integer_feature").unwrap().unwrap();
-    match val {
-        Value::Int(v) => assert!(v == 200),
-        _ => assert!(false),
-    }
-
-    let val = client.get_value("boolean_feature").unwrap().unwrap();
-    match val {
-        Value::Bool(v) => assert!(v == TEST_FLAG_VALUE),
-        _ => assert!(false),
-    }
-}
-
-#[test]
-fn test_get_user_value() {
-    let val = Client::new(API_KEY)
-        .get_user_value(&test_user(), TEST_FEATURE_NAME)
+    // Then
+    let all_flags = flagsmith
+        .get_identity_flags("test_identity", None)
         .unwrap()
-        .unwrap();
-    match val {
-        Value::String(v) => assert!(v == TEST_USER_FEATURE_VALUE),
-        _ => assert!(false),
-    }
+        .all_flags();
+    assert_eq!(all_flags.len(), 1);
+    assert_eq!(all_flags[0].feature_name, fixtures::FEATURE_1_NAME);
+    assert_eq!(all_flags[0].feature_id, fixtures::FEATURE_1_ID);
+    assert_eq!(
+        all_flags[0].value_as_string().unwrap(),
+        fixtures::FEATURE_1_STR_VALUE
+    );
+    api_mock.assert();
 }
 
-#[test]
-fn test_get_traits() {
-    let traits = Client::new(API_KEY)
-        .get_traits(&test_user(), vec![])
-        .unwrap();
-    assert!(traits.len() == 2)
+#[rstest]
+fn test_get_identity_flags_calls_api_when_no_local_environment_no_traits(
+    mock_server: MockServer,
+    identities_json: serde_json::Value,
+) {
+    // Given
+    let identifier = "test_identity";
+    let api_mock = mock_server.mock(|when, then| {
+        when.method(POST)
+            .path("/api/v1/identities/")
+            .header("X-Environment-Key", ENVIRONMENT_KEY)
+            .json_body(serde_json::json!({
+                "identifier": identifier,
+                "traits": []
+            }));
+        then.status(200).json_body(identities_json);
+    });
+    let url = mock_server.url("/api/v1/");
+    let flagsmith_options = FlagsmithOptions {
+        api_url: url,
+        ..Default::default()
+    };
+    let flagsmith = Flagsmith::new(ENVIRONMENT_KEY.to_string(), flagsmith_options);
+
+    // When
+
+    let all_flags = flagsmith
+        .get_identity_flags(identifier, None)
+        .unwrap()
+        .all_flags();
+
+    // Then
+    assert_eq!(all_flags.len(), 1);
+    assert_eq!(all_flags[0].feature_name, fixtures::FEATURE_1_NAME);
+    assert_eq!(all_flags[0].feature_id, fixtures::FEATURE_1_ID);
+    assert_eq!(
+        all_flags[0].value_as_string().unwrap(),
+        fixtures::FEATURE_1_STR_VALUE
+    );
+
+    api_mock.assert();
 }
 
-#[test]
-fn test_get_trait() {
-    let t = Client::new(API_KEY)
-        .get_trait(&test_user(), TEST_TRAIT_NAME)
-        .unwrap();
-    assert!(t.value == TEST_TRAIT_VALUE)
-}
+#[rstest]
+fn test_get_identity_flags_calls_api_when_no_local_environment_with_traits(
+    mock_server: MockServer,
+    identities_json: serde_json::Value,
+) {
+    // Given
+    let identifier = "test_identity";
+    let trait_key = "trait_key1";
+    let trait_value = "trai_value1";
 
-#[test]
-fn test_update_trait() {
-    let client = Client::new(API_KEY);
-    let mut old_trait = client
-        .get_trait(&different_user(), TEST_TRAIT_NAME)
-        .unwrap();
+    let api_mock = mock_server.mock(|when, then| {
+        when.method(POST)
+            .path("/api/v1/identities/")
+            .header("X-Environment-Key", ENVIRONMENT_KEY)
+            .header("content-type", "application/json")
+            .json_body(serde_json::json!({
+                "identifier": identifier,
+                "traits": [{"trait_key":trait_key, "trait_value": trait_value}]
+            }));
+        then.status(200).json_body(identities_json);
+    });
+    let url = mock_server.url("/api/v1/");
+    let flagsmith_options = FlagsmithOptions {
+        api_url: url,
+        ..Default::default()
+    };
+    let flagsmith = Flagsmith::new(ENVIRONMENT_KEY.to_string(), flagsmith_options);
 
-    old_trait.value = String::from(TEST_TRAIT_NEW_VALUE);
-    let updated = client.update_trait(&different_user(), &old_trait).unwrap();
-    assert!(TEST_TRAIT_NEW_VALUE == updated.value);
+    // When
+    let traits = vec![Trait {
+        trait_key: trait_key.to_string(),
+        trait_value: FlagsmithValue {
+            value: trait_value.to_string(),
+            value_type: FlagsmithValueType::String,
+        },
+    }];
+    let all_flags = flagsmith
+        .get_identity_flags(identifier, Some(traits))
+        .unwrap()
+        .all_flags();
 
-    let t = client
-        .get_trait(&different_user(), TEST_TRAIT_NAME)
-        .unwrap();
-    assert!(TEST_TRAIT_NEW_VALUE == t.value);
+    // Then
+    assert_eq!(all_flags.len(), 1);
+    assert_eq!(all_flags[0].feature_name, fixtures::FEATURE_1_NAME);
+    assert_eq!(all_flags[0].feature_id, fixtures::FEATURE_1_ID);
+    assert_eq!(
+        all_flags[0].value_as_string().unwrap(),
+        fixtures::FEATURE_1_STR_VALUE
+    );
 
-    old_trait.value = String::from("old value");
-    client.update_trait(&different_user(), &old_trait).unwrap();
+    api_mock.assert();
 }
